5e4abf45bfcb15214751a4a998358ab8caee3cf0
Dagisong <dagisongytofficial@gmail.com>
2022-07-17 10:43:10 +0700

    kirim file

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const vm = new Vue({
+    el: '#app',
+    data: member
+}) 
+
+const member = {
+    // string, number,boolean,array, object
+    name : 'Doremi Fasolasido',
+    email : 'doremi@gmail.com'
+
+}
